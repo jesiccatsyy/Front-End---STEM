f83cfb7d4ec924ac2e8325474df0614b6120381a
jeje <jesiccaanastasya5@gmail.com>
2025-09-14 08:27:17 +0700

    add number base conversion logic in script.js

--- script.js
+++ script.js
@@ -4,17 +4,37 @@ document.getElementById("convertBtn").addEventListener("click", () => {
     const input = document.getElementById("inputValue").value.trim();
 
     if (!input) {
-        document.getElementById("result").textContent = "Please enter a number!!!.";
+        document.getElementById("result").textContent = "⚠ Please enter a number!.";
         document.getElementById("steps").textContent = "";
         return;
     }
 
     if (from === to) {
-        document.getElementById("result").textContent = "Choose diffent number system!!!.";
+        document.getElementById("result").textContent = "⚠ Choose diffent number system!.";
         document.getElementById("steps").textContent = "";
         return;
     }
 
-    document.getElementById("result").textContent = "Conversion logic coming soon...";
-    document.getElementById("steps").textContent = "";
+    let decimalValue = parseInt(input, getBase(from));
+    if (isNaN(decimalValue)) {
+        document.getElementById("result").textContent = "⚠ Invalid unput for " + from + "!";
+        document,getElementById("steps").textContent = "";
+        return;
+    }
+
+    let convertedValue = decimalValue.toString(getBase(to)).toUpperCase();
+
+    document.getElementById("result").textContent = convertedValue;
+    document.getElementById("steps").textContent =
+    `Step 1: Convert ${input} (${from}) → ${decimalValue} (decimal)\n` +
+    `Step 2: Convert ${decimalValue} (decimal) → ${convertedValue} (${to})`;
 });
+
+function getBase(system) {
+    switch (system) {
+        case "binary": return 2;
+        case "octal": return 8;
+        case "decimal": return 10;
+        case "hexadecimal": return 16;
+    }
+}
